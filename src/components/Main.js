70653008ccf08ccda9ebcce2a5a019ea8c8ffa11
HarshKhandeparkar <mjkgoa@gmail.com>
2020-10-29 21:22:18 +0530

    add open file button for future use

--- src/components/Main.js
+++ src/components/Main.js
@@ -10,11 +10,14 @@ function Main() {
       </nav>
       <div className="container center">
         <p>
-          Open Source, Cross Platform Whiteboard software made with React, Electron and GPUJS Real Renderer.
+          Open Source, Cross Platform Whiteboard software made with React JS, Electron and GPU.JS Real Renderer.
         </p>
 
         <NavLink to="/pages" className="btn center brand-gradient gradient-text">
-          <i className="material-icons">brush</i> Start Writing
+          <i className="material-icons left">brush</i>Start New
+        </NavLink>
+        <NavLink to="/" className="btn center brand-gradient gradient-text">
+          <i className="material-icons left">folder_open</i>Open Saved File
         </NavLink>
       </div>
     </div>
