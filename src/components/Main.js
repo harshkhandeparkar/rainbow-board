eb749754fae45bfdfcd889534464fb0806a29265
HarshKhandeparkar <mjkgoa@gmail.com>
2020-10-29 20:23:50 +0530

    even more ui changes

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,22 @@
+import { NavLink } from 'react-router-dom';
+
 function Main() {
   return (
-    <div className="container-fluid">
+    <div id="main">
       <nav>
         <div className="nav-wrapper header white">
-          <span className="logo-text brand-logo center">Rainbow Board</span>
+          <span className="logo-text brand-logo center brand-gradient">Rainbow Board</span>
         </div>
       </nav>
+      <div className="container center">
+        <p>
+          Open Source, Cross Platform Whiteboard software made with React, Electron and GPUJS Real Renderer.
+        </p>
+
+        <NavLink to="/pages" className="btn center brand-gradient">
+          <i className="material-icons">brush</i> Start Writing
+        </NavLink>
+      </div>
     </div>
   )
 }
