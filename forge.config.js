ce843ef344e03ce3b7e971d748c6a4c7b1e5c16d
HarshKhandeparkar <mjkgoa@gmail.com>
2020-11-02 17:46:54 +0530

    fix: replace ignore function with regex

--- forge.config.js
+++ forge.config.js
@@ -2,31 +2,10 @@ const { join } = require('path');
 
 module.exports = {
   packagerConfig: {
-    // ignore: function (path) {
-    //   console.log('lol')
-    //   return true
-    //   if (
-    //       path.startsWith(join('/', 'node_modules', 'electron-is-dev')) ||
-    //       path.startsWith(join('/', 'node_modules', 'electron-squirrel-startup')) ||
-    //       path.startsWith(join('/', 'public', 'electron.js')) ||
-    //       path.startsWith(join('/', 'package.json')) ||
-    //       path.startsWith(join('/', 'LICENSE')) ||
-    //       path.startsWith(join('/', 'build'))
-    //   ) {
-    //     console.log(path);
-    //     return false;
-    //   }
-    //   else return true;
-
-    //   // return (
-    //   //   !path.startsWith(join('/', 'node_modules', 'electron-is-dev')) &&
-    //   //   !path.startsWith(join('/', 'public')) &&
-    //   //   !path.startsWith(join('/', 'package.json')) &&
-    //   //   !path.startsWith(join('/', 'LICENSE')) &&
-    //   //   !path.startsWith(join('/', 'dist')) &&
-    //   //   !path.startsWith(join('/', '.git'))
-    //   // )
-    // }
+    ignore: [
+      // Thanks to https://regex101.com
+      '^node_modules\/(?!electron-is-dev|electron-squirrel-startup)[a-zA-Z\/\.\\]*'
+    ]
   },
   makers: [
     {
