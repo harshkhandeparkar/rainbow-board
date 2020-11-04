320acba99913407e1c6fd9de0247f90c1d4e6500
HarshKhandeparkar <mjkgoa@gmail.com>
2020-11-04 15:32:27 +0530

    fix: include module ms in the binary

--- forge.config.js
+++ forge.config.js
@@ -2,7 +2,7 @@ module.exports = {
   packagerConfig: {
     ignore: [
       // Thanks to https://regex101.com
-      '^[\/]?node_modules\/(?!electron-is-dev|electron-squirrel-startup)[a-zA-Z\/\.\\\-]*',
+      '^[\/]?node_modules\/(?!electron-is-dev|electron-squirrel-startup|ms)[a-zA-Z\/\.\\\-]*',
       '^[\/]?src/[a-zA-Z\/\.\\\-]*',
       '^[\/]?forge.config.js',
       '^[\/]?public\/(?!electron.js)[a-zA-Z\/\.\\\-]*'
