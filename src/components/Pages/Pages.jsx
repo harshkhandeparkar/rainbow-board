4a4dbb65b76f34a7a6b4b150320752a9cfd96291
HarshKhandeparkar <shadowwarriorpro3003@gmail.com>
2021-03-23 01:06:57 +0530

    feat: page number and delete button

--- src/components/Pages/Pages.jsx
+++ src/components/Pages/Pages.jsx
@@ -11,8 +11,7 @@ export class Pages extends Component {
 
     this.state = {
       currentPage: 0,
-      pagesLength: 1,
-      mode: 'paint'
+      pagesLength: 1
     }
 
     this.pageRef = createRef();
@@ -26,15 +25,36 @@ export class Pages extends Component {
           <button
             className="btn-floating right page-btn"
             onClick={this.state.currentPage === this.state.pagesLength - 1 ? this.addPage : this.nextPage}
+            title={this.state.currentPage === this.state.pagesLength - 1 ? 'Add Page' : 'Next Page'}
           >
             <i className={`brand-text fa fa-${this.state.currentPage === this.state.pagesLength - 1 ? 'plus' : 'chevron-right'}`} />
           </button>
+
+          <span
+            className="btn-floating page-btn top-left brand-text"
+            style={{ fontWeight: 'bold' }}
+            title="Page Number"
+          >
+            {this.state.currentPage + 1} / {this.state.pagesLength}
+          </span>
+
+          {
+            this.state.pagesLength > 1 &&
+            <button
+              className="btn-floating page-btn top-right"
+              onClick={this.deletePage}
+              title="Delete This Page"
+            >
+              <i className="brand-text fa fa-trash" />
+            </button>
+          }
 
           {
             this.state.currentPage !== 0 &&
             <button
               className="btn-floating left page-btn"
               onClick={this.lastPage}
+              title="Previous page"
             >
               <i className="fa fa-chevron-left brand-text" />
             </button>
@@ -84,6 +104,30 @@ export class Pages extends Component {
     this.setState({
       currentPage: this.state.currentPage - 1
     })
+  }
+
+  deletePage = (e) => {
+    e.preventDefault();
+
+    if (this.state.pagesLength > 1) {
+      const board = this.pageRef.current.state.boardState.drawBoard;
+      this.pages = this.pages.filter((value, index) => index !== this.state.currentPage); // Delete that page
+      console.log(this.pages);
+
+      if (this.state.currentPage === this.state.pagesLength - 1) {
+        board.importData(this.pages[this.state.currentPage - 1]);
+        this.setState({
+          currentPage: this.state.currentPage - 1,
+          pagesLength: this.state.pagesLength - 1
+        })
+      }
+      else {
+        board.importData(this.pages[this.state.currentPage]);
+        this.setState({
+          pagesLength: this.state.pagesLength - 1
+        })
+      }
+    }
   }
 }
 
