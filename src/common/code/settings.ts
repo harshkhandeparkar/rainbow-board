8ff2c1cf27a5c9ee18840ebd5c9810a2d10146a8
HarshKhandeparkar <shadowwarriorpro3003@gmail.com>
2021-04-03 15:24:24 +0530

    feat: optional setting caching

--- src/common/code/settings.ts
+++ src/common/code/settings.ts
@@ -13,28 +13,32 @@ import {
 
 export class SettingManager<SettingType> {
   key: string;
+  cache: boolean;
   value: SettingType;
 
   constructor(
     key: string,
-    defaultValue: SettingType
+    defaultValue: SettingType,
+    cache: boolean = true
   ) {
     this.key = key;
+    this.cache = cache;
 
     if (!hasSync(key)) this.set(defaultValue as any);
-    this.value = <unknown>getSync(this.key) as SettingType;
+    if (this.cache) this.value = <unknown>getSync(this.key) as SettingType;
   }
 
   get(): SettingType {
-    return this.value;
+    if (this.cache) return this.value;
+    else return <unknown>getSync(this.key) as SettingType;
   }
 
   set(value: SettingType) {
-    this.value = value;
+    if (this.cache) this.value = value;
     setSync(this.key, value as any);
   }
 }
 
-export const startFullscreenSetting = new SettingManager<START_FULLSCREEN_SETTING>(START_FULLSCREEN_SETTING_KEY, START_FULLSCREEN_SETTING_DEFAULT);
-export const startMaximizedSetting = new SettingManager<START_MAXIMIZED_SETTING>(START_MAXIMIZED_SETTING_KEY, START_MAXIMIZED_SETTING_DEFAULT);
-export const showMenuBarWhenFullscreenSetting = new SettingManager<SHOW_MENU_BAR_WHEN_FULLSCREEN_SETTING>(SHOW_MENU_BAR_WHEN_FULLSCREEN_SETTING_KEY, SHOW_MENU_BAR_WHEN_FULLSCREEN_SETTING_DEFAULT);
+export const startFullscreenSetting = new SettingManager<START_FULLSCREEN_SETTING>(START_FULLSCREEN_SETTING_KEY, START_FULLSCREEN_SETTING_DEFAULT, false);
+export const startMaximizedSetting = new SettingManager<START_MAXIMIZED_SETTING>(START_MAXIMIZED_SETTING_KEY, START_MAXIMIZED_SETTING_DEFAULT, false);
+export const showMenuBarWhenFullscreenSetting = new SettingManager<SHOW_MENU_BAR_WHEN_FULLSCREEN_SETTING>(SHOW_MENU_BAR_WHEN_FULLSCREEN_SETTING_KEY, SHOW_MENU_BAR_WHEN_FULLSCREEN_SETTING_DEFAULT, false);
