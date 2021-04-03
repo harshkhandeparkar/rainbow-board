79712c4c49433a1653d655793e3033bd428f54d6
HarshKhandeparkar <shadowwarriorpro3003@gmail.com>
2021-04-03 18:51:24 +0530

    feat: update what's new page

--- src/renderer/components/WhatsNew/WhatsNew.tsx
+++ src/renderer/components/WhatsNew/WhatsNew.tsx
@@ -22,9 +22,9 @@ function New(
 ) {
   return (
     <GridItem options={{width: size}}>
-      <div className="card full-height-card center">
+      <div className="card full-height-card">
         <div className="card-content">
-          <span className="card-title styled-text brand-text">{title}</span>
+          <span className="card-title styled-text brand-text center">{title}</span>
           <p>{desc}</p>
         </div>
         <div className="card-action">
@@ -71,22 +71,53 @@ export default class WhatsNew extends Component {
             size={1}
           />
           <New
-            title="Themes And Settings"
-            desc="Instead of just two themes - Light and Dark, a settings page has been added where the theme can be chosen. Two new themes have also been added -
-            Dark Whiteboard (Board is white but UI dark) and Light Blackboard (Board is dark but UI light)."
+            title="MSI"
+            desc="
+            Since the EXE file for windows is sometimes considered harmful(even though it is not), a .msi installer file has been added and
+             will be provided for v0.7.0 and future versions.
+            "
             version="v0.7.0"
-            size={2}
+            size={1}
           />
           <New
-            title="UI Cleanup And Redesign"
-            desc={<span>
-              The UI has been cleaned up. Reduced shadows and transitions. Uses cleaner borders and the themes are more consistent
-              New icons from <a href="https://fontawesome.com">FontAwesome v5</a>. The new UI tries to follow the <a href="https://developer.gnome.org/hig/stable/">GNOME Human Interface Guidelines</a>.
-              There are tooltips to most buttons with shortcut keys displayed. The top menu only displays what is necessary and some new items have been added to it.
-            </span>}
+            title="More Shortcuts"
+            desc={<span>More shortcuts have been added. You can find them out in the menu bar. The most important one is &quot;Ctrl + Space&quot; or &quot;Cmd + Space&quot; on Mac to switch to the previous tool. Suggested by Pffft on the <a href={packageFile.discordInvite}>Discord Server</a>.</span>}
+            version="v0.7.0"
+            size={1}
+          />
+
+          <New
+            title="Themes And Settings"
+            desc={
+              <span>
+                Instead of just two themes - Light and Dark, a settings page has been added where the theme can be chosen. <br />
+                Two new themes have also been added: <br />
+                <b>Dark Whiteboard</b>: (Board is white but UI dark) <br />
+                <b>Light Blackboard</b>: (Board is dark but UI light). <br /> <br />
+                Three other settings have also been added: <br />
+                <b>Start Maximized</b>: App starts maximized if this settings is checked. <br />
+                <b>Start Fullscreen</b>: App starts fullscreen if this settings is checked. <br />
+                <b>Show Menu Bar When Fullscreen</b>: App menu bar is displayed in fullscreen if this setting is checked.
+              </span>
+            }
             version="v0.7.0"
             size={3}
           />
+
+          <New
+            title="UI Cleanup And Redesign"
+            desc={
+              <span>
+                The new UI tries to follow the <a href="https://developer.gnome.org/hig/stable/">GNOME Human Interface Guidelines</a> <br />
+                The UI has been cleaned up. Reduced shadows and transitions. Uses cleaner borders and the themes are more consistent. New icons from <a href="https://fontawesome.com">FontAwesome v5</a>. <br />
+                There are tooltips to most buttons with shortcut keys displayed. The top menu only displays what is necessary and some new items have been added to it. <br />
+                The brush and line colors are displayed as a thin line under the tool's button (Suggested by Pffft on the <a href={packageFile.discordInvite}>Discord Server</a>). The scrollbar has also been tweaked to look consistent with the theme.
+              </span>
+            }
+            version="v0.7.0"
+            size={3}
+          />
+
           <New
             title="Desktop App Features"
             desc="The app has been changed to look more like a desktop app than a web app running in a wrapper.
