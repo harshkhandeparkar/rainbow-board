12c074ed6a7c9d5f98da5325a13d6b1175fad25e
vivek singh <vikky30singh@gmail.com>
2021-04-03 15:38:07 +0530

    added js for the materialize components

---
+++ public/js/helper.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+    $('.materialboxed').materialbox();
+    $('.scrollspy').scrollSpy();
+    $('.btn').click(function(e){
+        e.preventDefault();
+    });
+});
