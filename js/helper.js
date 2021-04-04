e7fda81b7f1fc5cb84b323c5ee927d23d1a15e96
vivek singh <vikky30singh@gmail.com>
2021-04-04 17:25:38 +0530

    removed all the extra download buttons from top

--- js/helper.js
+++ js/helper.js
@@ -1,13 +1,8 @@
 $(document).ready(function(){
-    let downloadButtons = 'none'
     $('.sidenav').sidenav();
     $('.materialboxed').materialbox();
     $('.scrollspy').scrollSpy();
     $('.btn').click(function(e){
         e.preventDefault();
-    });
-    $('#main-dwnld-btn').on('click', () => {
-        downloadButtons === 'none' ? downloadButtons = 'block' : downloadButtons = 'none' ;
-        $('#download-btns').css('display', downloadButtons);
     });
 });
