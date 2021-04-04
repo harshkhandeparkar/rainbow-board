bdd599ee24e4026189dce321ec933da9f9bfe243
vivek singh <vikky30singh@gmail.com>
2021-04-04 16:33:50 +0530

    added dynamic behaviour to download buttons

--- js/helper.js
+++ js/helper.js
@@ -1,8 +1,13 @@
 $(document).ready(function(){
+    let downloadButtons = 'none'
     $('.sidenav').sidenav();
     $('.materialboxed').materialbox();
     $('.scrollspy').scrollSpy();
     $('.btn').click(function(e){
         e.preventDefault();
+    });
+    $('#main-dwnld-btn').on('click', () => {
+        downloadButtons === 'none' ? downloadButtons = 'block' : downloadButtons = 'none' ;
+        $('#download-btns').css('display', downloadButtons);
     });
 });
