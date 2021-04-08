64738884c2d991baa32c2838d844e48a1312bbd3
vivek singh <vikky30singh@gmail.com>
2021-04-08 21:03:41 +0530

    removed redundant code from download.js

--- js/download.js
+++ js/download.js
@@ -35,39 +35,34 @@ if (navigator) {
 }
 
 const showDownloadPage = () => {
+
+  // Set display to none for all platforms.
+  linuxElement.style.display = 'none';
+  windowsElement.style.display = 'none';
+  macElement.style.display = 'none';
+  noDownloadElement.style.display = 'none';
+
   switch(os) {
     case 'linux':
       linuxElement.style.display = 'block';
-      windowsElement.style.display = 'none';
-      macElement.style.display = 'none';
-      noDownloadElement.style.display = 'none';
 
       linuxElement.querySelector('#appimg-link').setAttribute('href', info.downloadURLs.appimg);
       linuxElement.querySelector('#deb-link').setAttribute('href', info.downloadURLs.deb);
       linuxElement.querySelector('#zip-lin-link').setAttribute('href', info.downloadURLs.zip_linux);
       break;
     case 'windows':
-      linuxElement.style.display = 'none';
       windowsElement.style.display = 'block';
-      macElement.style.display = 'none';
-      noDownloadElement.style.display = 'none';
 
       windowsElement.querySelector('#exe-link').setAttribute('href', info.downloadURLs.exe);
       windowsElement.querySelector('#msi-link').setAttribute('href', info.downloadURLs.msi);
       windowsElement.querySelector('#zip-win-link').setAttribute('href', info.downloadURLs.zip_windows);
       break;
     case 'mac':
-      linuxElement.style.display = 'none';
-      windowsElement.style.display = 'none';
       macElement.style.display = 'block';
-      noDownloadElement.style.display = 'none';
 
       macElement.querySelector('#dmg-link').setAttribute('href', info.downloadURLs.dmg);
       break;
     default:
-      linuxElement.style.display = 'none';
-      windowsElement.style.display = 'none';
-      macElement.style.display = 'none';
       noDownloadElement.style.display = 'block';
       break;
   }
