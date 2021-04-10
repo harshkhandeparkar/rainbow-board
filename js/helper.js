f69cfe2dc1670d1c59d7eac0fa6149e26b47c47f
Harsh Khandeparkar <34770591+HarshKhandeparkar@users.noreply.github.com>
2021-04-10 12:48:57 +0530

    fix: make a.btn clickable

--- js/helper.js
+++ js/helper.js
@@ -1,8 +1,5 @@
 $(document).on('DOMContentLoaded', function(){
     $('.sidenav').sidenav();
-    $('.btn').on('click', function(e){
-        e.preventDefault();
-    });
     $('.dropdown-trigger').dropdown();
     $('.carousel.carousel-slider').carousel({
         indicators: false,
