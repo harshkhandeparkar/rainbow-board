7e19c1a0c5b5418904d9a1c7cf275671cfc40e21
HarshKhandeparkar <shadowwarriorpro3003@gmail.com>
2021-04-27 19:19:13 +0530

    feat: update what's new page

--- src/renderer/components/WhatsNew/WhatsNew.tsx
+++ src/renderer/components/WhatsNew/WhatsNew.tsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { SAVE, EXPORT_PAGE } from '../../../common/constants/shortcuts';
 
 import { changelogSetting } from '../../util/settings';
 import packageFile from '../../../../package.json';
@@ -60,74 +61,29 @@ export default class WhatsNew extends Component {
           className="container"
         >
           <New
-            title="Save And Load"
-            desc="You can now save the whole whiteboard as a .rainbow file and load it."
-            version="v0.7.0"
-            size={1}
+            title="Changed Shortcuts"
+            desc={`${SAVE.platformFormattedString} is now used to save the whiteboard as a ".rainbow" file and ${EXPORT_PAGE.platformFormattedString} to export a single page as an image.`}
+            version="v0.7.1"
           />
           <New
-            title="MSI"
-            desc="
-            Since the EXE file for windows is sometimes considered harmful(even though it is not), a .msi installer file has been added and
-             will be provided for v0.7.0 and future versions.
-            "
-            version="v0.7.0"
-            size={1}
-          />
-          <New
-            title="More Shortcuts"
-            desc={<span>More shortcuts have been added. You can find them out in the menu bar. The most important one is &quot;Ctrl + Space&quot; or &quot;Cmd + Space&quot; on Mac to switch to the previous tool. Suggested by Pffft on the <a href={packageFile.discordInvite}>Discord Server</a>.</span>}
-            version="v0.7.0"
-            size={1}
-          />
-
-          <New
-            title="Themes And Settings"
+            title="Mouse Magick"
             desc={
               <span>
-                Instead of just two themes - Light and Dark, a settings page has been added where the theme can be chosen. <br />
-                Two new themes have also been added: <br />
-                <b>Dark Whiteboard</b>: (Board is white but UI dark) <br />
-                <b>Light Blackboard</b>: (Board is dark but UI light). <br /> <br />
-                Three other settings have also been added: <br />
-                <b>Start Maximized</b>: App starts maximized if this settings is checked. <br />
-                <b>Start Fullscreen</b>: App starts fullscreen if this settings is checked. <br />
-                <b>Show Menu Bar When Fullscreen</b>: App menu bar is displayed in fullscreen if this setting is checked.
+                Your mouse coupled with the keyboard can do magick. It can now be used to change the size of the brush, zoom and pan.
+                <br /><br />
+                Just moving the scroll wheel will change the brush size.<br />
+                But if you hold control key on the keyboard, you can do the following:<br />
+                <b>Ctrl + Scroll</b>: Zoom in/out of the whiteboard.<br />
+                <b>Ctrl + Drag the board with mouse</b>: Will move (pan) a zoomed in whiteboard.<br />
               </span>
             }
-            version="v0.7.0"
-            size={3}
-          />
-
-          <New
-            title="UI Cleanup And Redesign"
-            desc={
-              <span>
-                The new UI tries to follow the <a href="https://developer.gnome.org/hig/stable/">GNOME Human Interface Guidelines</a> <br />
-                The UI has been cleaned up. Reduced shadows and transitions. Uses cleaner borders and the themes are more consistent. New icons from <a href="https://fontawesome.com">FontAwesome v5</a>. <br />
-                There are tooltips to most buttons with shortcut keys displayed. The top menu only displays what is necessary and some new items have been added to it. <br />
-                The brush and line colors are displayed as a dot in the tool's button (Suggested by Pffft on the <a href={packageFile.discordInvite}>Discord Server</a>). The scrollbar has also been tweaked to look consistent with the theme.
-              </span>
-            }
-            version="v0.7.0"
-            size={3}
-          />
-
-          <New
-            title="Desktop App Features"
-            desc="The app has been changed to look more like a desktop app than a web app running in a wrapper.
-            A new menu has been added along with other changes. Starting from now, the web app will not be updated beyond
-            v0.5.4 because maintaining both is not possible. New optimizations and features will be added to the desktop app and the website
-            will be used as an about page.
-            "
-            version="v0.6.0"
+            version="v0.7.1"
             size={2}
           />
           <New
-            title="Hotter Keys"
-            desc="New hotkeys (keyboard shortcuts) have been added to almost every action such as adding pages, changing the page, color palette and choosing the tool.
-            Use the top menu to find out which hotkey does what."
-            version="v0.6.0"
+            title="Visible Eraser"
+            desc="The eraser now has a dotted outline to show how large it is."
+            version="v0.7.1"
             size={1}
           />
         </Grid>
