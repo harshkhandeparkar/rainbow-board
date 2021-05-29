4bfe02d0f3272831f4cc6ac4f5d6aa61a76b4394
HarshKhandeparkar <shadowwarriorpro3003@gmail.com>
2021-05-29 16:06:22 +0530

    feat: cleanup of what's new page

--- src/renderer/components/WhatsNew/WhatsNew.tsx
+++ src/renderer/components/WhatsNew/WhatsNew.tsx
@@ -1,21 +1,22 @@
 import React, { Component } from 'react';
-import { SAVE, EXPORT_PAGE } from '../../../common/constants/shortcuts';
-
+import { eq } from 'semver';
 import packageFile from '../../../../package.json';
 
 import Grid from '../Grid/Grid';
 import GridItem from '../Grid/GridItem';
 
+import { Icon } from '../Icon/Icon';
+import { faExclamationCircle } from '@fortawesome/free-solid-svg-icons/faExclamationCircle';
+
 import { Header } from '../Header/Header';
 
-const { version, discordInvite, repository } = packageFile;
+const { version: currentVersion, discordInvite, repository } = packageFile;
 
 function New(
-  {title, desc, version, link, size = 1}: {
+  {title, desc, version, size = 1}: {
     title: string | JSX.Element,
     desc: string | JSX.Element,
     version: string,
-    link?: string,
     size?: number
   }
 ) {
@@ -23,18 +24,32 @@ function New(
     <GridItem options={{width: size}}>
       <div className="card full-height-card">
         <div className="card-content">
-          <span className="card-title styled-text brand-text center">{title}</span>
+          <span className="card-title styled-text brand-text center">
+            {title}
+          </span>
           <p>{desc}</p>
         </div>
         <div className="card-action">
-          Added in <a target="_blank" rel="noreferrer" href={`https://github.com/HarshKhandeparkar/rainbow-board/releases/${version}`}>v{version.replace('v', '')}</a>
+          New in&nbsp;
+          <a
+            target="_blank"
+            rel="noreferrer"
+            style={{marginRight: '5px'}}
+            href={`${repository}/releases/${version}`}
+          >
+            v{version.replace('v', '')}
+          </a>
+          {
+            eq(version, currentVersion) && <Icon
+              options={{
+                icon: faExclamationCircle,
+                className: '',
+                title: 'Current Version',
+                size: 'sm'
+              }}
+            />
+          }
         </div>
-        {
-          link &&
-          <div className="card-action">
-            <a target="_blank" rel="noreferrer" href={link}>Link</a>
-          </div>
-        }
       </div>
     </GridItem>
   )
@@ -56,20 +71,6 @@ export default class WhatsNew extends Component {
           className="container"
         >
           <New
-            title="That Annoying Bug Is Gone"
-            desc={
-              <span>
-                Line tool used to leave circles behind after using. These circles are now gone. <br />
-                This bug was there for 12 whole days! v0.7.1 was released 12 days ago! <br />
-                No one reported this until I found it myself. If you don't know, you can always report the bugs in our Discord server or on Github Issues using the following links: <br />
-                <a href={discordInvite} target="_blank" rel="noreferrer">Discord Server</a>. <br/>
-                <a href={repository + '/issues'} target="_blank" rel="noreferrer">Github Issues</a>. <br/>
-              </span>
-            }
-            version="v0.7.2"
-            size={3}
-          />
-          <New
             title="Mouse Magick"
             desc={
               <span>
@@ -83,44 +84,6 @@ export default class WhatsNew extends Component {
             }
             version="v0.7.1"
             size={2}
-          />
-          <New
-            title="Changed Shortcuts"
-            desc={`${SAVE.platformFormattedString} is now used to save the whiteboard as a ".rainbow" file and ${EXPORT_PAGE.platformFormattedString} to export a single page as an image.`}
-            version="v0.7.1"
-          />
-          <New
-            title="Mouse Magick"
-            desc={
-              <span>
-                Your mouse coupled with the keyboard can do magick. It can now be used to change the size of the brush, zoom and pan.
-                <br /><br />
-                Just moving the scroll wheel will change the brush size.<br />
-                But if you hold control key on the keyboard, you can do the following:<br />
-                <b>Ctrl + Scroll</b>: Zoom in/out of the whiteboard.<br />
-                <b>Ctrl + Drag the board with mouse</b>: Will move (pan) a zoomed in whiteboard.<br />
-              </span>
-            }
-            version="v0.7.1"
-            size={2}
-          />
-          <New
-            title="Visible Eraser"
-            desc="The eraser now has a dotted outline to show how large it is."
-            version="v0.7.1"
-            size={1}
-          />
-          <New
-            title="Background Type"
-            desc="You can now select a background for the whiteboard - Ruled, Grid or Blank - using the buttons in te botton toolbar."
-            version="v0.7.1"
-            size={1}
-          />
-          <New
-            title="System Font"
-            desc="Rainbow Board now uses the operating system's default font to make it look more consistent."
-            version="v0.7.1"
-            size={1}
           />
         </Grid>
       </div>
